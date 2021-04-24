a2f4824747fea612b8c84daf63cd114c3f43ada8
Turan Karatuğ <tkaratug@hotmail.com.tr>
2021-04-24 17:20:15 +0300

    Iys requests added.

---
+++ src/Iys/AbstractNetgsmIys.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TarfinLabs\Netgsm\Iys;
+
+use TarfinLabs\Netgsm\NetgsmApiClient;
+
+abstract class AbstractNetgsmIys extends NetgsmApiClient
+{
+    protected string $url = '';
+
+    protected string $method = 'GET';
+
+    protected array $body = [];
+
+    /**
+     * Send request.
+     *
+     * @return string
+     */
+    public function send(): string
+    {
+        $response = $this->client->request($this->method, $this->url, [
+            'headers'   => [
+                'username'  => $this->credentials['user_code'],
+                'password'  => $this->credentials['secret'],
+                'brandCode' => $this->credentials['brand_code'],
+            ],
+            'json'      => $this->body
+        ]);
+
+        return $response->getBody()->getContents();
+    }
+}
---
+++ src/Iys/NetgsmIys.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace TarfinLabs\Netgsm\Iys;
+
+use TarfinLabs\Netgsm\Iys\Requests\Add;
+use TarfinLabs\Netgsm\Iys\Requests\Search;
+
+class NetgsmIys extends AbstractNetgsmIys
+{
+    /**
+     * Add address request.
+     *
+     * @param Add $request
+     * @return $this
+     */
+    public function addAddress(Add $request): NetgsmIys
+    {
+        $this->url = $request->getUrl();
+        $this->body = $request->body();
+        $this->method = 'POST';
+
+        return $this;
+    }
+
+    /**
+     * Search address request.
+     *
+     * @param Search $request
+     * @return $this
+     */
+    public function searchAddress(Search $request): NetgsmIys
+    {
+        $this->url = $request->getUrl();
+        $this->body = $request->body();
+        $this->method = 'POST';
+
+        return $this;
+    }
+}
